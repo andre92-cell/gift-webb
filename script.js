7a5d33d9257bcbc7d1c9ba4a50d223a6051e88ff
andre92-cell <mhmmadandre92@gmail.com>
2026-05-25 19:01:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const btn = document.getElementById("giftBtn");
+const giftBox = document.getElementById("giftBox");
+const text = document.getElementById("text");
 
-btn.onclick = () => {
-  alert("Hadiah terbuka ✨");
+giftBox.onclick = () => {
+
+  giftBox.classList.add("open");
+
+  text.innerHTML = "Hadiah terbuka 💖";
+
+  setTimeout(() => {
+    document.body.innerHTML = `
+    
+    <div style="
+      height:100vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      flex-direction:column;
+      background:black;
+      color:white;
+      font-family:sans-serif;
+    ">
+
+      <h1 style="font-size:50px;">
+        Surprise ✨
+      </h1>
+
+      <p style="margin-top:20px;font-size:22px;">
+        Kamu spesial 💖
+      </p>
+
+    </div>
+
+    `;
+  }, 1000);
+
 };
